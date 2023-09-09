d64636030e7d7767ecaa06d3658f70b4c76185de
Mykhailo Zhykhariev <mykhailo_zhykhariev@vfc.com>
2023-09-09 17:46:04 +0300

    feat(index-page): inject layout into app

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>
-    <NoteList />
-    <SingleNote />
+  <div class="notes">
+    <NoteList class="note-list"/>
+    <SingleNote class="single-note" />
   </div>
 </template>
 
-<script setup lang="ts">
+<script  lang="ts">
 import NoteList from '~/components/NoteList.vue';
 import SingleNote from '~/components/SingleNote.vue';
+
+export default {
+  name: 'notes',
+  components: {
+    NoteList,
+    SingleNote
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/variables.scss';
+
+.notes {
+  display: flex;
+}
+
+.note-list {
+  flex: 1; 
+  max-width: 30%;
+  border-right: 1px solid $border-color;
+}
+
+.single-note {
+  flex: 1; 
+  padding: 20px; 
+}
+</style>
